ab32d904e584b37c8dc5ab614d4dc2142ae48404
praveen-analyze <epraveen952@example.com>
2026-03-25 18:39:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import InputBox from "./InputBox";
+import Contents from "./Contents";
+import About from "./About";
+import Footer from "./Footer";
+
+function App(){
+    return(
+        <div>   
+    <Navbar />
+    <InputBox />
+    <Contents />
+    <About />
+    <Footer />
+  </div>
+
+    )
+}
+
+export default App
